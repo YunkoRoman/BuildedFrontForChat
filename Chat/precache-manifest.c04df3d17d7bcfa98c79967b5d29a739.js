30e529300c34b39da8366bfe4ead705851b3cce4
YunkoRoman <romanyunko1996@gmail.com>
2021-05-24 12:38:56 +0300

    new v

---
+++ Chat/precache-manifest.c04df3d17d7bcfa98c79967b5d29a739.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "286938326dabd445b7ca51f4168c519d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56114038561a3a6491d8",
+    "url": "/static/css/main.53671e76.chunk.css"
+  },
+  {
+    "revision": "944f99105c17eda6bebc",
+    "url": "/static/js/2.ddbecf84.chunk.js"
+  },
+  {
+    "revision": "f3a69483bc670b2da5d044f5dce3c392",
+    "url": "/static/js/2.ddbecf84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56114038561a3a6491d8",
+    "url": "/static/js/main.db4d87fd.chunk.js"
+  },
+  {
+    "revision": "dcc36446341eee41c8f3",
+    "url": "/static/js/runtime-main.3047f782.js"
+  }
+]);
